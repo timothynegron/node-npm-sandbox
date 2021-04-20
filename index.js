b970bbe0092d79d63abbfab786430934409be216
Timothy Negron <timothynegron.eet@gmail.com>
2021-04-20 14:15:04 -0400

    Adds random supervillain log

---
+++ index.js
@@ -0,0 +1,11 @@
+const superheroes = require("superheroes");
+
+const mySuperhero = superheroes.random();
+
+console.log(mySuperhero);
+
+const supervillains = require("supervillains");
+
+const mySupervillain = supervillains.random();
+
+console.log(mySupervillain);
